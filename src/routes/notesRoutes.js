42053080a8bff74e8b6bd29bb7b217211b13e0c4
roman-yakubovskyi <yakubovskyi133@gmail.com>
2026-05-31 09:36:00 +0200

    fixed project

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
-
+import { celebrate } from 'celebrate';
+import {
+  getAllNotesSchema,
+  noteIdSchema,
+  createNoteSchema,
+  updateNoteSchema,
+} from '../validations/notesValidation.js';
 import {
   getAllNotes,
   getNoteById,
@@ -10,14 +16,14 @@ import {
 
 const router = Router();
 
-router.get('/notes', getAllNotes);
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 
-router.get('/notes/:noteId', getNoteById);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 
-router.post('/notes', createNote);
+router.post('/notes', celebrate(createNoteSchema), createNote);
 
-router.patch('/notes/:noteId', updateNote);
+router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
-router.delete('/notes/:noteId', deleteNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
 
 export default router;
